3a1b307363ad7685d1410d4253d5e5199635fc0f
Arunomi <vypirovsky@gmail.com>
2021-02-16 17:40:23 +0200

    added product section

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,8 @@ function scripts() {
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'app/js/main.js'
+        'app/js/main.js',
+        'node_modules/mixitup/dist/mixitup.js'
     ]) 
         .pipe(concat('main.min.js'))
         .pipe(uglify()) 

40288e585b65596fc927a05002ba691081d651f8
Arunomi <vypirovsky@gmail.com>
2021-02-16 19:58:38 +0200

    added filter, slider, why, author, tutorial

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,22 @@
 
 $(function () {
-  
-    var mixer = mixitup('.products__inner-box');
+
     
+  
+    var mixer = mixitup('.products__inner-box'); //filter
+
+    $('.product-slider__inner').slick({  //slickslider
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+    });
+
+    $(".rate-star").rateYo({        //rating
+        rating: 3.6,
+        starWidth: "12px",
+        ratedFill: "#ffa726",
+        readOnly: true        
+    });
 });
